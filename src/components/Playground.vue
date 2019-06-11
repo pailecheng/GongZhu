e8969513a1fd9abb2ce4d563f5ab5b8c560502cb
Aries Cs <aries0d0f@gmail.com>
2019-06-11 18:36:03 +0800

    Layout: stream socket

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,16 +1,52 @@
 <template>
   <div>
-    TEST
+    <input type="text" v-model="playerName">
+    <button @click="joinGame()">Join Game</button>
+    Room: {{ room }}
   </div>
 </template>
 
 <script>
+import uuid from 'uuid/v1'
+
 export default {
   name: 'Playground',
+
+  data () {
+    return {
+      playerName: '',
+      room: ''
+    }
+  },
 
   sockets: {
     connect () {
       console.log('socket connected')
+    }
+  },
+
+  methods: {
+    joinGame () {
+      const id = uuid()
+      this.$socket.emit('JOIN', {
+        name: this.playerName,
+        id
+      })
+      this.sockets.subscribe(`PLAYER-${id}`, data => {
+        this.setChannel(data.id)
+      })
+    },
+
+    setChannel (id) {
+      this.sockets.subscribe(`PLAYER-${id}`)
+      this.sockets.subscribe('ROOM', data => {
+        this.joinRoom(data)
+      })
+    },
+
+    joinRoom (data) {
+      this.sockets.unsubscribe('ROOM')
+      this.sockets.subscribe(`ROOM-${data}`)
     }
   }
 }
