b517ea637742a2fa5fe7d53ffc88bdba46cb46cc
Aries Cs <aries0d0f@gmail.com>
2019-06-11 17:08:43 +0800

    Fix: socket client syntax

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
-import VueSocketio from 'vue-socket.io'
-import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
 
 import App from './App'
 import store from './store'
 
-Vue.use(VueSocketio, socketio(`http://localhost:3000/`), store)
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('http://localhost:3000'),
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
 
 const app = new Vue({
   store,
