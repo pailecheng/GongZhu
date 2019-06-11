c8f5d4d13df77ded2c14da9a901b3131c89833ce
Aries Cs <aries0d0f@gmail.com>
2019-06-11 16:08:18 +0800

    Methods: let router get masked card

---
+++ server/router.ts
@@ -0,0 +1,30 @@
+import * as express from 'express'
+import * as path from 'path'
+
+import { game } from './app'
+import { UUID } from './util/uuid'
+import { IRoom } from './model/room'
+import { ICard } from './model/card'
+
+
+const router = express.Router()
+
+router
+  .get('/:room/card/:id', (req: express.Request, res: express.Response): void => {
+    try {
+      const roomId: UUID = req.params.room
+      const cardId: UUID = req.params.id
+      const targetRoom: IRoom = game.rooms.filter((room) => (roomId === room.ID))[0]
+      const targetCard: ICard = targetRoom.dealer.originCardSets.filter((card) => (cardId === card.ID))[0]
+
+      res.status(200)
+      res.sendFile(path.resolve(__dirname, `./public/img/${targetCard.serialNum}.gif`))
+
+    } catch (error) {
+      res.status(404)
+      res.send(error)
+      throw new Error(error)
+    }
+  })
+
+export default router
